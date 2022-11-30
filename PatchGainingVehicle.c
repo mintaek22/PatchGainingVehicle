c84da6cb1e896e56d3eb3fbfc95cdce95f78866d
chweyun <chweyun>
2022-11-30 22:15:16 +0900

    rotation

--- PatchGainingVehicle.c
+++ PatchGainingVehicle.c
@@ -436,17 +436,40 @@ void update_action(void){
 	int detect = get_stat(DETECT);
 	int move = get_stat(MOVE);
 
-	
+	if (move == 2)
+	{
+		int col_left = getColorName(cs_left);
+		int col_middle = getColorName(cs_middle);
+		int col_right = getColorName(cs_right);
 
-	if(move == 2){
-		/*
-			code here!
-		*/
-		set_stat(MOVE,1);
+		if (dir_cur == dir_dest)
+		{ // 0 rotation
+			return;
+		}
+		else if ((dir_cur + dir_dest) % 2 == 1 && (dir_cur - dir_dest == -1 || 3))
+		{ // 90 rotation
+			set_motor(10, -10);
+			sleep(1700);
+			set_motor(0, 0);
+		}
+		else if ((dir_cur + dir_dest) % 2 == 1 && (dir_cur - dir_dest == 1 || -3))
+		{ // -90 rotation
+			set_motor(-10, 10);
+			sleep(1700);
+			set_motor(0, 0);
+		}
+		else if ((dir_cur + dir_dest) % 2 == 0)
+		{ // 180 rotation
+			set_motor(10, -10);
+			sleep(3300);
+			set_motor(0, 0);
+		}
+		dir_cur = dir_dest;
+		set_stat(MOVE, 1);
 		move = get_stat(MOVE);
 		return;
 	}
-	
+
 	if(move == 3){
 		if(ongrid_warigari-- > 6){
 			set_motor(0.2*SPEED_MAX,-0.2*SPEED_MAX);
